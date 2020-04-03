e10619b3a6b2eac1579865a61e13a49dce299be1
root-lucas <945832032@qq.com>
2020-04-03 09:52:58 +0800

    add: data validation

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -10,13 +10,30 @@ class UsersCtl {
         ctx.body = db
     }
     async findById(ctx) {
+        if (ctx.params.id * 1 >= db.length) {
+            ctx.throw(412, '先决条件失败: id 大于用户列表长度')
+        }
         ctx.body = db[ctx.params.id * 1]
     }
     async create(ctx) {
+        // 校验数据
+        ctx.verifyParams({
+            name: { type: 'string', required: true },
+            age: { type: 'number', required: false }
+        })
+
         db.push(ctx.request.body)
         ctx.body = ctx.request.body
     }
     async update(ctx) {
+        if (ctx.params.id * 1 >= db.length) {
+            ctx.throw(412, '先决条件失败: id 大于用户列表长度')
+        }
+        // 校验数据
+        ctx.verifyParams({
+            name: { type: 'string', required: true },
+            age: { type: 'number', required: false }
+        })
         db[ctx.params.id * 1] = ctx.request.body
         ctx.body = ctx.request.body
     }
--- app/index.js
+++ app/index.js
@@ -7,8 +7,19 @@ const Koa = require('koa')
 const app = new Koa()
 const bodyparser = require('koa-bodyparser')
 const routing = require('./routes')
+const error = require('koa-json-error')
+const parameter = require('koa-parameter')
+
+
+const env = process.env.NODE_ENV
+app.use(error({
+    // 生产环境不返回 stack (泄露服务器目录信息)
+    postFormat: (err, { stack, ...rest }) => env === 'production' ? rest : { stack, ...rest }
+}))
+
 
 app.use(bodyparser())
+app.use(parameter(app))
 routing(app)
 
 // 监听端口
