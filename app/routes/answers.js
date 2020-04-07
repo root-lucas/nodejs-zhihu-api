66c06324b2e501ce9471a60090bdbb9299f84225
root-lucas <945832032@qq.com>
2020-04-07 11:27:49 +0800

    add: answer CRUD api

---
+++ app/routes/answers.js
@@ -0,0 +1,29 @@
+/*
+ * @Description  : 答案路由
+ * @Author       : lucas
+ */
+
+const jwt = require('koa-jwt')
+const Router = require('koa-router')
+const router = new Router({ prefix: '/questions/:questionId/answers' })
+const {
+    find, findById, create, update, delete: del,
+    checkAnswerExist, checkAnswerer,
+} = require('../controllers/answers')
+
+const { secret } = require('../config')
+
+const auth = jwt({ secret })
+
+// 获取答案列表
+router.get('/', find)
+// 新建答案
+router.post('/', auth, create)
+// 获取特定答案
+router.get('/:id', checkAnswerExist, findById)
+// 修改答案
+router.patch('/:id', auth, checkAnswerExist, checkAnswerer, update)
+// 删除答案
+router.delete('/:id', auth, checkAnswerExist, checkAnswerer, del)
+
+module.exports = router
